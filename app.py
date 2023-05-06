6d8c63c358755226f928a378571524522821d467
thanhlamkma <vtlam134@gmail.com>
2023-05-07 01:25:46 +0700

    Add layout, pages

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 from flask import Flask, render_template
+from flask_paginate import Pagination, get_page_args, get_page_parameter
 import json
 
-app=Flask(__name__,template_folder='templates')
-@app.route('/')
-def show_data():
-    # Load JSON data from the file
-    with open('email.json') as json_file:
-        data = json.load(json_file)
+app = Flask(__name__, template_folder="templates", static_url_path="/static")
 
-    # Render the template and pass the data to the template
-    return render_template('index.html', data=data)
+def get_emails():
+  with open('email.json') as json_file:
+    emails = json.load(json_file)
+    
+  return emails
+
+def get_pagination_emails(offset=0, emails_per_page=10):
+  emails = get_emails()
+  return emails[offset: offset + emails_per_page]
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route('/apply')
+def show_emails():
+  emails = get_emails()
+
+  # Pagination
+  page, emails_per_page, offset = get_page_args(page_parameter="page", per_page_parameter="per_page")
+  
+  total_emails = len(emails)
+  
+  pagination_emails = get_pagination_emails(offset=offset, emails_per_page=emails_per_page)
+  
+  pagination = Pagination(page=page, per_page=emails_per_page, total=total_emails, css_framework="bootstrap4", alignment="center")
+
+  # Render the template and pass the messages to the template
+  return render_template("apply.html", emails=pagination_emails, page=page, emails_per_page=emails_per_page, pagination=pagination)
 
 if __name__ == '__main__':
-    app.run()
+  app.run(debug=True)
